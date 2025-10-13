02c1bebd6e72ccaf06769f85fa00576d55795932
Vasyl Kovalov <raid3r81@gmail.com>
2025-10-13 19:39:03 +0300

    Добавлен запрос пароля и улучшен код
    В коде реализован запрос пароля у пользователя, который теперь отправляется на сервер вместе с именем. Обновлены вызовы методов для отправки и получения строк с использованием класса `SocketHelper`. Исправлен формат вывода времени сообщений. Удалены неиспользуемые методы и закомментированные участки, что улучшило читаемость кода.

--- SocketClientP35/Program.cs
+++ SocketClientP35/Program.cs
@@ -1,4 +1,5 @@
-﻿using ChatModels.Models;
+﻿using ChatModels;
+using ChatModels.Models;
 using System.Net;
 using System.Net.Sockets;
 
@@ -13,6 +14,10 @@ int port = 5000;
 // Запитаємо у користувача його ім'я
 Console.Write("Введіть ваше ім'я: ");
 string userName = Console.ReadLine();
+
+// Запитаємо у користувача його пароль
+Console.Write("Введіть ваш пароль: ");
+string password = Console.ReadLine();
 
 Task.Run(() =>
 {
@@ -47,19 +52,32 @@ void GetMessages()
     socket.Connect(new IPEndPoint(ipAdress, port));
 
     // Відправка імені
-    SendString(socket, userName);
+    SocketHelper.SendString(socket, userName);
     // Отримання відповіді ОК
-    string response = ReceiveString(socket);
+    string response = SocketHelper.ReceiveString(socket);
+
+    // Відправити пароль
+     SocketHelper.SendString(socket, password);
+    // Отримання відповіді ОК / FAIL
+    response = SocketHelper.ReceiveString(socket);
+    if (response != "OK")
+    {
+        Console.WriteLine("Невірний пароль");
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+        return;
+    }
+
     // Відправка запиту на отримання повідомлень "GET_MESSAGES"
-    SendString(socket, "GET_MESSAGES");
+    SocketHelper.SendString(socket, "GET_MESSAGES");
     // отримання повідомлень
-    string messagesJson = ReceiveString(socket);
+    string messagesJson = SocketHelper.ReceiveString(socket);
     // десеріалізація
     var messages = System.Text.Json.JsonSerializer.Deserialize<List<ChatMessage>>(messagesJson);
     // вивід на екран
     foreach (var message in messages)
     {
-        Console.WriteLine($"{message.Timestamp.ToString("dd.MM.yyyy H:m:s")} {message.From.Username}: {message.Text}");
+        Console.WriteLine($"{message.Timestamp.ToString("dd.MM.yyyy HH:mm:ss")} {message.From.Username}: {message.Text}");
     }
     socket.Shutdown(SocketShutdown.Both);
     // Завершення з'єднання
@@ -73,76 +91,29 @@ void SendMessage(ChatMessage chatMessage)
     Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
     socket.Connect(new IPEndPoint(ipAdress, port));
     // Відправка імені
-    SendString(socket, userName);
+    SocketHelper.SendString(socket, userName);
     // Отримання відповіді ОК
-    string response = ReceiveString(socket);
+    string response = SocketHelper.ReceiveString(socket);
+    
+    // Відправити пароль
+    SocketHelper.SendString(socket, password);
+    // Отримання відповіді ОК / FAIL
+    response = SocketHelper.ReceiveString(socket);
+    if (response != "OK")
+    {
+        Console.WriteLine("Невірний пароль");
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+        return;
+    }
     // Відправка запиту на відправку повідомлень "SEND_MESSAGE"
-    SendString(socket, "SEND_MESSAGE");
+    SocketHelper.SendString(socket, "SEND_MESSAGE");
     // Відправка повідомлення
     string messageJson = System.Text.Json.JsonSerializer.Serialize(chatMessage);
-    // Отримання відповіді ОК
-    SendString(socket, messageJson);
+    SocketHelper.SendString(socket, messageJson);
+    // Отримання відповіді ОК / FAIL
+    response = SocketHelper.ReceiveString(socket);
     // Завершення з'єднання
     socket.Shutdown(SocketShutdown.Both);
     socket.Close();
-}
-
-
-
-
-//await workerAsync();
-
-
-//async Task workerAsync()
-//{
-//    while (true)
-//    {
-//        try
-//        {
-//            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-//            socket.Connect(new IPEndPoint(ipAdress, port));
-
-//            Console.WriteLine($"Підключено до сервера: {socket.RemoteEndPoint}");
-
-//            // Передача даних серверу
-//            string message = "Привіт від клієнта!";
-//            byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
-//            socket.Send(data);
-
-//            // Отримання даних від сервера
-//            byte[] buffer = new byte[256];
-//            int bytesRead = socket.Receive(buffer);
-//            string receivedMessage = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
-//            Console.WriteLine($"Отримано повідомлення від сервера: {receivedMessage}");
-
-//            socket.Shutdown(SocketShutdown.Both);
-//            socket.Close();
-//            Console.WriteLine("Відключено від сервера.");
-//            await Task.Delay(1000);
-
-//        }
-//        catch (Exception ex)
-//        {
-//            Console.Write(ex.Message);
-//        }
-//    }
-   
-//}
-
-
-//Console.ReadLine();
-
-
-void SendString(Socket socket, string message)
-{
-    byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
-    socket.Send(data);
-}
-
-string ReceiveString(Socket socket)
-{
-    byte[] buffer = new byte[2048];
-    int bytesRead = socket.Receive(buffer);
-    string receivedMessage = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
-    return receivedMessage;
 }
