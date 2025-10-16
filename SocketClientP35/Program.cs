3e9760c7b724247e97456ac6578db908ec3d9835
Vasyl Kovalov <raid3r81@gmail.com>
2025-10-16 18:51:35 +0300

    Добавить поддержку загрузки и скачивания файлов
    Внесены изменения в обработку файлов и взаимодействие с пользователем. Реализованы новые методы `SendFile` и `DownloadFile` для отправки и получения файлов через сокеты. Улучшен вывод сообщений в консоль, добавлена проверка существования директории для загруженных файлов и обработка пароля пользователя при подключении к серверу.

--- SocketClientP35/Program.cs
+++ SocketClientP35/Program.cs
@@ -19,6 +19,13 @@ string userName = Console.ReadLine();
 Console.Write("Введіть ваш пароль: ");
 string password = Console.ReadLine();
 
+
+var uploadDir = "UploadedFiles";
+if (!Directory.Exists(uploadDir))
+{
+    Directory.CreateDirectory(uploadDir);
+}
+
 Task.Run(() =>
 {
     while (true)
@@ -32,6 +39,23 @@ Task.Run(() =>
 while (true)
 {
     var text = Console.ReadLine();
+
+    if (text == "/screenshot")
+    {
+        var filename = @"C:\Users\kvvkv\source\repos\SocketServerP35\SocketServerP35\Program.cs";
+        SendFile(filename);
+
+        SendMessage(new ChatMessage
+        {
+            From = new ChatUser { Username = userName },
+            Timestamp = DateTime.Now,
+            Text = $"Відправив файл: {Path.GetFileName(filename)}",
+            Filename = Path.GetFileName(filename)
+        });
+
+        continue;
+    }
+
     if (!string.IsNullOrEmpty(text))
     {
         SendMessage(new ChatMessage
@@ -77,7 +101,13 @@ void GetMessages()
     // вивід на екран
     foreach (var message in messages)
     {
+        
         Console.WriteLine($"{message.Timestamp.ToString("dd.MM.yyyy HH:mm:ss")} {message.From.Username}: {message.Text}");
+        if (!string.IsNullOrEmpty(message.Filename))
+        {
+            DownloadFile(message.From.Username, message.Filename);
+            Console.WriteLine($"Файл {message.Filename} завантажено у папку {Path.Combine(uploadDir, message.From.Username)}");
+        }
     }
     socket.Shutdown(SocketShutdown.Both);
     // Завершення з'єднання
@@ -113,6 +143,99 @@ void SendMessage(ChatMessage chatMessage)
     SocketHelper.SendString(socket, messageJson);
     // Отримання відповіді ОК / FAIL
     response = SocketHelper.ReceiveString(socket);
+    // Завершення з'єднання
+    socket.Shutdown(SocketShutdown.Both);
+    socket.Close();
+}
+
+
+
+void SendFile(string filename)
+{
+    // Приєднання до сервера
+    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    socket.Connect(new IPEndPoint(ipAdress, port));
+    // Відправка імені
+    SocketHelper.SendString(socket, userName);
+    // Отримання відповіді ОК
+    string response = SocketHelper.ReceiveString(socket);
+
+    // Відправити пароль
+    SocketHelper.SendString(socket, password);
+    // Отримання відповіді ОК / FAIL
+    response = SocketHelper.ReceiveString(socket);
+    if (response != "OK")
+    {
+        Console.WriteLine("Невірний пароль");
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+        return;
+    }
+    
+    SocketHelper.SendString(socket, "UPLOAD_FILE");
+
+    response = SocketHelper.ReceiveString(socket); // "FILENAME"
+
+    SocketHelper.SendString(socket, Path.GetFileName(filename));
+    SocketHelper.SendFile(socket, filename);
+
+    // Завершення з'єднання
+    socket.Shutdown(SocketShutdown.Both);
+    socket.Close();
+}
+
+
+void DownloadFile(string fromUser, string filename)
+{
+    // Приєднання до сервера
+    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    socket.Connect(new IPEndPoint(ipAdress, port));
+    // Відправка імені
+    SocketHelper.SendString(socket, userName);
+    // Отримання відповіді ОК
+    string response = SocketHelper.ReceiveString(socket);
+    // Відправити пароль
+    SocketHelper.SendString(socket, password);
+    // Отримання відповіді ОК / FAIL
+    response = SocketHelper.ReceiveString(socket);
+    if (response != "OK")
+    {
+        Console.WriteLine("Невірний пароль");
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+        return;
+    }
+    SocketHelper.SendString(socket, "DOWNLOAD_FILE");
+    
+    response = SocketHelper.ReceiveString(socket);
+    Console.WriteLine(response);
+    //SocketHelper.SendString(clientSocket, "FROM");
+    SocketHelper.SendString(socket, fromUser);
+    
+    response = SocketHelper.ReceiveString(socket);
+    Console.WriteLine(response);
+    //SocketHelper.SendString(clientSocket, "FILENAME");
+    SocketHelper.SendString(socket, filename);
+    response = SocketHelper.ReceiveString(socket);
+    Console.WriteLine(response);
+    if (response != "EXISTS")
+    {
+        Console.WriteLine("File not exists on server");
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+        return;
+    }
+
+    SocketHelper.SendString(socket, "OK");
+
+    var downloadFilePath = Path.Combine(uploadDir, fromUser, filename);
+    if (!Directory.Exists(Path.Combine(uploadDir, fromUser)))
+    {
+        Directory.CreateDirectory(Path.Combine(uploadDir, fromUser));
+    }
+
+    SocketHelper.ReceiveFile(socket, downloadFilePath);
+
     // Завершення з'єднання
     socket.Shutdown(SocketShutdown.Both);
     socket.Close();
